8a584aa143249ef848c2fbe45660992962a919f6
bobertskey <72881899+bobertskey@users.noreply.github.com>
2023-07-19 11:35:25 -0400

    Update streamlit_app.py
    Trying some small changes to see if it reads.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -8,7 +8,8 @@ streamlit.text('🥣 Omega 3 & BLueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
